1268c888fdf36cc834ef5ea026fd0587c242c876
Ali-hassan-yousaf <alihassan.tech27@gmail.com>
2025-06-02 12:15:14 -0700

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -24,7 +24,7 @@ const Sidebar = () => {
         </NavLink>
         <NavLink to={'/add-Saloon'} className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''}`}>
           <img className='min-w-5' src={assets.add_icon} alt='' />
-          <p className='hidden md:block'>Precautions</p>
+          <p className='hidden md:block'>Precautions & Risk</p>
         </NavLink>
 
         <NavLink to={'/meal'} className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''}`}>
